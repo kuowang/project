82877856af9773d8c28a76cea05b82aa76e19fdf
wangkuo <everup@163.com>
2019-07-18 14:26:29 +0800

    公告的列表新增编辑删除效果修改

--- resources/views/base/notice/index.blade.php
+++ resources/views/base/notice/index.blade.php
@@ -38,7 +38,7 @@
                     <div class="title">
                         公告列表<a id="dynamicTable"></a>
                         @if(in_array(600202,$pageauth))
-                        <a class="btn btn-success" title="新增公告"  onclick="addNotice()">
+                        <a class="btn btn-success" title="新增公告" id="addnotice"  onclick="addNotice()">
                             <i class="layui-icon">新增公告</i>
                         </a>
                         @endif
@@ -118,7 +118,39 @@
                 </div>
             </div>
         </div>
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">新增</h4>
+                    </div>
+                    <div class="modal-body">
 
+                        <div class="form-group">
+                            <label for="txt_departmentname">部门名称</label>
+                            <input type="text" name="txt_departmentname" class="form-control" id="txt_departmentname" placeholder="部门名称">
+                        </div>
+                        <div class="form-group">
+                            <label for="txt_parentdepartment">上级部门</label>
+                            <input type="text" name="txt_parentdepartment" class="form-control" id="txt_parentdepartment" placeholder="上级部门">
+                        </div>
+                        <div class="form-group">
+                            <label for="txt_departmentlevel">部门级别</label>
+                            <input type="text" name="txt_departmentlevel" class="form-control" id="txt_departmentlevel" placeholder="部门级别">
+                        </div>
+                        <div class="form-group">
+                            <label for="txt_statu">描述</label>
+                            <input type="text" name="txt_statu" class="form-control" id="txt_statu" placeholder="状态">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span>关闭</button>
+                        <button type="button" id="btn_submit" class="btn btn-primary" data-dismiss="modal"><span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span>保存</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
     <style>
@@ -127,7 +159,18 @@
         }
     </style>
 
+
+
+
     <script>
+
+        //注册新增按钮的事件
+
+        function addNotice(){
+            $("#myModalLabel").text("新增");
+            $('#myModal').modal();
+        }
+        //$('#myModal').modal();
         //一般直接写在一个js文件中
 
             function editNotice(id){
@@ -142,7 +185,7 @@
                     });
                 });
             }
-            function addNotice(){
+            function addNotice1(){
                 layui.use(['layer', 'form'], function(){
                     var layer = layui.layer
                         ,form = layui.form;
