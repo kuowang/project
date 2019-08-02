e179e3e65e751a4a1c445684864ccca9ab27137b
wangkuo <everup@163.com>
2019-08-02 17:39:37 +0800

    统一相同的参数放到user方法中同意调取

--- app/Http/Controllers/Project/ProjectController.php
+++ app/Http/Controllers/Project/ProjectController.php
@@ -29,38 +29,54 @@ class ProjectController extends WebController
     public function projectStart(Request $request)
     {
         $this->user();
-        $search =$request->input('search','');
+        $project_name =$request->input('project_name','');
+        $address =$request->input('address','');
+        $customer_leader =$request->input('customer_leader','');
+        $success_level =$request->input('success_level',0);
+
         $page =$request->input('page',1);
-        $rows =$request->input('rows',40);
-        $data['search']        =$search;
-        $data['data'] =$this->getProjectList($search,$page,$rows);
+        $rows =$request->input('rows',4);
+        $data['project_name']        =$project_name;
+        $data['address']        =$address;
+        $data['customer_leader']        =$customer_leader;
+        $data['success_level']        =$success_level;
+        $datalist=$this->getProjectList(0,$project_name,$address,$customer_leader,$success_level,$page,$rows);
+        $url='/project/projectStart?project_name='.$project_name.'&address='.$address.'&customer_leader='.$customer_leader.'&success_level='.$success_level;
+        $data['page']   =$this->webfenye($page,ceil($datalist['count']/$rows),$url);
+        $data['data']   =$datalist['data'];
 
         $data['navid']      =15;
         $data['subnavid']   =1502;
         $data['status']=$request->input('status',0); //1成功 2失败
         $data['notice']=$request->input('notice','成功'); //提示信息
-
         return view('project.index',$data);
     }
 
-    protected function getProjectList($search='',$page=1,$rows=20)
+    protected function getProjectList($status,$project_name='',$address='',$customer_leader='',$success_level=1,$page=1,$rows=20)
     {
-        if(empty($search)){
-            return DB::table('project')
-                ->leftjoin('engineering','project.id','=','project_id')
-                ->orderby('project.id','desc')
-                ->skip(($page-1)*$rows)
-                ->take($rows)
-                ->get();
-        }else{
-            return DB::table('project')
-                ->leftjoin('engineering','project.id','=','project_id')
-                ->where('project_name','like','%'.$search.'%')
-                ->orderby('project.id','desc')
-                ->skip(($page-1)*$rows)
-                ->take($rows)
-                ->get();
+        $db=DB::table('project')
+            ->leftjoin('engineering','project.id','=','project_id')
+            ->where('project.status',$status);
+
+        if(!empty($project_name)){
+            $db->where('project_name','like','%'.$project_name.'%');
         }
+        if(!empty($address)){
+            $db->where('address','like','%'.$address.'%');
+        }
+        if(!empty($customer_leader)){
+            $db->where('customer_leader','like','%'.$customer_leader.'%');
+        }
+        if(!empty($success_level)){
+            $db->where('success_level','like','%'.$success_level.'%');
+        }
+        $data['count'] =$db->count();
+        $data['data']= $db->orderby('project.id','desc')
+            ->orderby('engineering.id','asc')
+            ->skip(($page-1)*$rows)
+            ->take($rows)
+            ->get();
+        return $data;
     }
 
 
