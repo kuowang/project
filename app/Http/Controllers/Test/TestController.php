db17013b6a3fc99445a76388cc64ab0a585b209b
wangkuo <everup@163.com>
2019-09-19 11:43:37 +0800

    测试邮件发送

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\DB;
 use App\Models\SystemSetting;
 use PDF;
+use Mail;
 
 class TestController  extends WebController
 {
@@ -31,6 +32,18 @@ class TestController  extends WebController
         $pdf = PDF::loadView('test.testPdf', $data);
         return $pdf->stream();
         //return $pdf->download('invoice.pdf');
+    }
+
+
+    public function sendemail() {
+        $name = '我发的第一份邮件';
+        // Mail::send()的返回值为空，所以可以其他方法进行判断
+        Mail::raw('你好，我是PHP程序！', function ($message) {
+            $to = 'wangkuo@ontheroadstore.com';
+            $message ->to($to)->subject('纯文本信息邮件测试');
+        });
+        // 返回的一个错误数组，利用此可以判断是否发送成功
+        dd(Mail::failures());
     }
 
 }
