9409b98e503c015aac1a137d210a441a9e4a5a35
wangkuo <everup@163.com>
2019-10-17 18:33:02 +0800

    登录页面调整

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,79 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-    <style>
-        body{
-            background-position: center 0;
-            background-repeat: no-repeat;
-            background-color: #0076A6;
-            background-image: url(/img/login.jpg);
-            background-size: cover;
-            -webkit-background-size: cover;
-            height: 100%;
-            width: 100%;
-            background-attachment: fixed;
-        }
-    </style>
-<div class="container" style="margin-top: 200px;">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">登录</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
+    <link rel="stylesheet" href="/layui/css/login.css">
+    <link rel="stylesheet" href="/layui/css/amazeui.min.css">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">邮箱</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
+    <div class="am-g myapp-login">
+        <div class="myapp-login-bg">
+            <div data-am-widget="tabs" class="am-tabs am-tabs-d2">
+                <div class="am-tabs-bd">
+                    <div data-tab-panel-0 class="am-tab-panel am-active">
+                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                            {{ csrf_field() }}
+                            <fieldset>
+                                <div class="am-form-group">
+                                    <input type="text" id="doc-vld-name" name="email" placeholder="请输入账号" class="am-form-field" value="{{ old('email') }}"  required autofocus/>
                                 @if ($errors->has('email'))
-                                    <span class="help-block">
+                                        <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
+                                    @endif
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">密码</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
+                                </div>
+                                <div class="am-form-group">
+                                    <input id="doc-vld-name" type="password" placeholder="请输入密码"  class="form-control am-form-field"  name="password" required>
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
+                                    @endif
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : ''}}> 记住我
-                                    </label>
                                 </div>
-                            </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    登录
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                                <button class="myapp-login-button am-btn am-btn-secondary" type="submit"> 登      录</button>
+                            </fieldset>
+                        </form>
+                        <div id="footerinfo">公司名称电话号码：123123123</div>
+
+                    </div>
+
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 
 
