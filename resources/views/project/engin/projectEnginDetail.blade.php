15c6384ca7ff7fa336e871162708ba41df283ad9
wangkuo <everup@163.com>
2019-10-23 14:07:05 +0800

    内容显示优化

--- resources/views/project/engin/projectEnginDetail.blade.php
+++ resources/views/project/engin/projectEnginDetail.blade.php
@@ -50,6 +50,95 @@
                         {{$project->project_name}}
                     </div>
                 </div>
+                <div class="widget-body">
+                    <div class="clearfix"></div>
+                        <table class="layui-table layui-form">
+                            <thead>
+                            <tr><th colspan="8"><span class="btn btn-info">工程信息</span></th></tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td class="pro-title">工程名称{{$engin->id}}</td>
+                                <td> {{$engin->engineering_name}}</td>
+                                <td class="pro-title">工程地址</td>
+                                <td>{{$engin->engin_address}}</td>
+                                <td class="pro-title">建筑总面积（m²）</td>
+                                <td>{{$engin->build_area}}</td>
+                            </tr>
+                            <tr>
+                                <td class="pro-title">建筑总层数</td>
+                                <td>{{$engin->build_floor}}</td>
+                                <td class="pro-title">建筑总高度（m）</td>
+                                <td>{{$engin->build_height}}</td>
+                                <td class="pro-title">室内净高（最小）（m）</td>
+                                <td>{{$engin->indoor_height}}</td>
+                            </tr>
+                            <tr>
+                                <td class="pro-title">建筑数量(栋)</td>
+                                <td>{{$engin->build_number}}</td>
+                                <td colspan="4"></td>
+
+                            </tr>
+                            </tbody>
+                        </table>
+                        <div class="clearfix"></div>
+
+                        <table class="layui-table layui-form">
+                            <thead>
+                            <tr><th colspan="8"><span class="btn btn-info">负责人信息</span></th></tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td class="pro-title">销售负责人</td>
+                                <td>{{$engin->sale_username}}</td>
+                                <td class="pro-title">设计负责人</td>
+                                <td> {{$engin->design_username}}
+                                </td>
+
+                                <td class="pro-title">预算负责人</td>
+                                <td>
+                                    {{$engin->budget_username}}
+                                </td>
+                                <td class="pro-title">合约负责人</td>
+                                <td>{{$engin->technical_username}}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+
+                        <div class="clearfix"></div>
+                        <table class="layui-table layui-form">
+                            <thead>
+                            <tr>
+                                <th colspan="2"><span class="btn btn-info">工程动态信息</span></th>
+                            </tr>
+                            </thead>
+                            <tbody style="text-align: center" id="project_dongtai">
+                            <tr>
+                                <td style="width: 20%;">时间</td>
+                                <td style="width: 70%;">动态</td>
+                            </tr>
+
+                            @foreach($dynamic as $item)
+                                <tr>
+                                    <td  class="pro-title">
+                                        {{$item->dynamic_date}}
+                                    </td>
+                                    <td >
+                                        "{{$item->dynamic_content}}
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                        <div class="clearfix"></div>
+                        <div class="layui-form-item" style="float: right;clear: left">
+                            <a href="javascript:history.go(-1)">
+                                <label for="L_repass" class="layui-form-label"></label>
+                                <span class="btn btn-success" lay-filter="add" lay-submit="">返回/取消</span>
+                            </a>
+                        </div>
+                    <div class="clearfix"></div>
+                </div>
             </div>
         </div>
 
@@ -74,62 +163,5 @@
     <hr class="hr-stylish-1">
 </div>
 
-    <script>
-        //一般直接写在一个js文件中
-
-
-        //添加事件
-        function add_dongtai() {
-            str ='<tr>' +
-                '    <td  class="pro-title">' +
-                '        <input type="hidden"  name="dynamic_id[]" class="span8 "  value="0" lay-skin="primary" >' +
-                '        <input type="text"  name="dynamic_date[]"  placeholder="yyyy-MM-dd" class="span8 notempty dynamic_date"  value="" lay-skin="primary" >' +
-                '    </td>' +
-                '    <td >' +
-                '        <input type="text"  name="dynamic_content[]" class="span12 notempty"  value="" lay-skin="primary" >' +
-                '    </td>' +
-                '    <td><a class="btn btn-danger" onclick="deleteTrRow(this)">删除</a></td>' +
-                '</tr>';
-            $("#project_dongtai").append(str);
-            //添加日期点击事件
-            layui.use('laydate', function() {
-                var laydate = layui.laydate;
-                lay('.dynamic_date').each(function(){
-                    laydate.render({
-                        elem: this
-                        ,trigger: 'click'
-                    });
-                });
-            });
-            //点击文本框设置背景色
-            $(".notempty").focus(function(){
-                $(this).css("background-color","#fff");
-            });
-        }
-
-
-        //日期選擇
-        layui.use('laydate', function() {
-            var laydate = layui.laydate;
-            lay('.dynamic_date').each(function(){
-                laydate.render({
-                    elem: this
-                    ,trigger: 'click'
-                });
-            });
-        });
-        //点击只能输入数字
-        function key(th){
-            $(th).keyup(function(){
-                $(this).val($(this).val().replace(/[^0-9.]/g,''));
-            }).bind("paste",function(){  //CTR+V事件处理
-                $(this).val($(this).val().replace(/[^0-9.]/g,''));
-            }).css("ime-mode", "disabled"); //CSS设置输入法不可用
-            va =$(th).val();
-            if(va > 1000000000 || va < 0) {
-                $(th).val(0);
-            }
-        }
-    </script>
 
 @endsection
